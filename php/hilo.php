429e239b998de984aeb4afe8c907c48a58842971
adrian-af <adrian.afo@hotmail.com>
2022-07-28 13:23:28 +0200

    página del hilo con campo para añadir nuevo mensaje

--- php/hilo.php
+++ php/hilo.php
@@ -1,8 +1,54 @@
 <?php
     include "conector.php";
     session_start();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hilo</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+    <style>
+        form
+        {
+            width: 60%;
+        }
+    </style>
+    <script src="https://cdn.tiny.cloud/1/osvl1ulep3sk3lvbav27xthkna729y9e64bqcjh0aju5fczf/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+</head>
+<body>
+    
+
+<?php
     //coger de POST qué hilo es
     $IDHilo = 1;
+    
+        
+    if(isset($_POST['editordata'])) //Si se ha rellenado el campo de nuevo mensaje, entrará en este if
+    {
+        $emailUsuario = $_SESSION["welcome"];
+        $queryUser = "SELECT id FROM usuarios WHERE correo='$emailUsuario'";
+
+        $consulta = mysqli_query($conector, $queryUser);
+        
+        $idUsuario = mysqli_fetch_assoc($consulta);
+        $idUsuario = $idUsuario['id']; //se coge el id del usuario que tiene iniciada la sesión para vincularlo al mensaje
+
+        $texto = $_POST['editordata']; //lo escrito en el editor
+        $datetime = new DateTime('NOW'); //fecha actual
+        $datetime = $datetime->format('Y-m-d H:i:s');
+
+        $queryMensaje = "INSERT INTO mensajes (texto, usuario, hilo, fecha)
+                        VALUES ('$texto', $idUsuario, $IDHilo, '$datetime')";
+        $consulta = mysqli_query($conector, $queryMensaje);
+        
+    }
 
     $query = "SELECT * FROM hilos WHERE ID=$IDHilo";
     $temas = mysqli_query($conector, $query);
@@ -53,3 +99,18 @@
     }
 
 ?>
+    <form method="post" action="">
+        <textarea id="summernote" name="editordata"></textarea>
+        <button type="submit">Sumbit</button>
+    </form>
+
+    
+    <script>
+        $(document).ready(function() 
+        {
+            $('#summernote').summernote();
+        });
+    </script>
+
+</body>
+</html>
