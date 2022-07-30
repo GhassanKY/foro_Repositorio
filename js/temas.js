d07a09d33d755cd19d032e6883a0acfc36d59086
MAIKOL\HP <mgarrido2503@gmail.com>
2022-07-30 17:09:56 +0200

    sehunda pagina con base de datos

---
+++ js/temas.js
@@ -0,0 +1,29 @@
+let boton = document.querySelector(".searchbutton");
+let item = document.querySelector(".form-control");
+const search = document.querySelector(".form-control");
+let pfHeader = document.querySelector(".pfHeader");
+const PopLR = document.querySelector(".PopLR");
+let menu = document.querySelector(".imgMenu");
+let desplegable = document.querySelector(".list");
+let configButton = document.querySelector(".configButton")
+let x = document.querySelector(".x");
+let form = document.querySelector(".cajaPublicarNuevo")
+let boton1 = document.querySelector(".buttonCreate")
+let botontema = document.querySelector(".botonTemas")
+let divtema = document.querySelector(".divTema");
+
+
+boton.addEventListener('click', () => {
+    item.classList.toggle("feature-v");
+});
+
+menu.addEventListener('click', () => {
+    desplegable.classList.toggle("list-v")
+})
+botontema.addEventListener("click", ()=>{
+    divtema.classList.toggle("temas-v");
+});
+
+pfHeader.addEventListener('click', () => {
+    PopLR.classList.toggle("PopLR-v");
+})
--- php/register.php
+++ php/register.php
@@ -3,7 +3,7 @@
 
 include "conector.php";
 
-// header("location:index.php");
+// header("location: ../index.html");
 
 $mensaje = "";
 $nombre = $_POST["nombre"] ??  null;
@@ -12,10 +12,10 @@ $clave = $_POST["clave"] ??  null;
 $usuario = $_POST["usuario"]?? null ;
 // $destino = $_POST["src-file1"] ?? null;
 //agregar imagen
-$imagen = $_FILES['src-file1']['name'];
-$ruta = $_FILES['src-file1'] ['tmp_name'];
+$imagen = $_FILES['src-file1']['name'] ?? null;
+$ruta = $_FILES['src-file1'] ['tmp_name'] ?? null;
 $destino = "../image/".$imagen;
-copy($ruta, $destino);
+copy($ruta, $destino) ?? null;
 
 $query = "INSERT INTO usuarios(correo,nombreUsuario,clave,nombreCompleto,image_user)
           VALUES('$correo','$usuario','$clave','$nombre','$destino')";
