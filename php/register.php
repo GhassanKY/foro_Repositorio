9d4bba194714b914e32333c7161973d176f2f436
MAIKOL\HP <mgarrido2503@gmail.com>
2022-08-02 09:56:50 +0200

    k

--- php/register.php
+++ php/register.php
@@ -15,6 +15,7 @@ $usuario = $_POST["usuario"]?? null ;
 $imagen = $_FILES['src-file1']['name'] ?? null;
 $ruta = $_FILES['src-file1'] ['tmp_name'] ?? null;
 $destino = "../image/".$imagen;
+
 copy($ruta, $destino) ?? null;
 
 $query = "INSERT INTO usuarios(correo,nombreUsuario,clave,nombreCompleto,image_user)
