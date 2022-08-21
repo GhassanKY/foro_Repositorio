6f7695a744c49095c0aa96d30dc1405f413dba17
MAIKOL\HP <mgarrido2503@gmail.com>
2022-08-21 23:18:22 +0200

    hnj

--- FRONTEND/perfil.php
+++ FRONTEND/perfil.php
@@ -5,7 +5,7 @@ include "../BACKEND/BD_PERFIL.php";
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible"  content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/ba10ce0731.js" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
